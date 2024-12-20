715772e6fa766d40f554414ea6c36c2f9d7d1445
geodebreaker <geodebreaker@users.noreply.github.com>
2024-12-20 07:35:24 -0600

    c

--- site/terminal.js
+++ site/terminal.js
@@ -1,13 +1,27 @@
+const terminal = [];
+const terminalPrompt = '$';
+
 const log = (...x) => {
   console.log(...x);
 };
 
 function terminalState(x) {
   document.querySelector('#termcon').style.display = x ? 'block' : 'none';
+  if (x) document.querySelector('#termin').focus();
+}
+
+function makeTerminal() {
+  document.querySelector('#termout').innerText =
+    terminal.concat([terminalPrompt + ' ' + document.querySelector('#termin').value]).join('\n');
 }
 
 function handleKey(x) {
-  log(x.key);
+  if (x.key == 'Enter') {
+    terminal.push(terminalPrompt + ' ' + document.querySelector('#termin').value);
+    document.querySelector('#termin').value = '';
+  }
+  setTimeout(makeTerminal, 50);
 }
 
-terminalState(true)
+setInterval(makeTerminal, 300);
+terminalState(true);
